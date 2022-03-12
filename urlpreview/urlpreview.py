d2d5017b96791f66a8b02aa8512f8ac3ff731dd9
Nightscape Coffee <23473065+coffeebank@users.noreply.github.com>
2022-03-11 16:09:57 -0800

    urlpreview: v0.1.0 release
    Adds og:image support
    Adds multi links in message support
    Adds 404/no results indicator

--- urlpreview/urlpreview.py
+++ urlpreview/urlpreview.py
@@ -1,4 +1,4 @@
-from mautrix.api import HTTPAPI
+import mautrix.api
 from mautrix.types import RoomID, ImageInfo
 from mautrix.util.config import BaseProxyConfig, ConfigUpdateHelper
 from maubot import Plugin, MessageEvent
@@ -26,40 +26,50 @@ class UrlpreviewBot(Plugin):
     async def handler(self, evt: MessageEvent, matches: List[str]) -> None:
         await evt.mark_read()
         msgs = ""
+        count = 0
         for _, url_str in matches:
-          if len(msgs) >= MAX_LINKS:
-            msgs.append("...")
+          if count >= MAX_LINKS:
             break
 
           appid = self.config["appid"]
           url_params = urllib.parse.urlencode({"i": url_str, "appid": appid})
-          embed_content =  "https://matrix-client.matrix.org/_matrix/media/r0/preview_url?url={}".format(url_str)
+          embed_content =  "https://matrix.org/_matrix/media/r0/preview_url?url={}".format(url_str)
           resp = await self.http.get(embed_content, headers={"Authorization":"Bearer {}".format(appid)})
-          if resp.status == 500:
-            await evt.reply("Hmmmm")
-            return None
+
+          # Guard clause
           if resp.status != 200:
-            self.log.warning(f"Error: {resp.status}")
-            return None
+            continue
           cont = json.loads(await resp.read())
 
+          # Set empty if no og:description
           if cont.get('og:description', None) == None:
             embed_desc = ""
           else:
-            embed_desc = cont.get('og:description', '')
+            embed_desc = str(cont.get('og:description', '')).replace('\r', ' ').replace('\n', ' ')
 
-          if cont.get('og:title', False):
+          # If there is an og:title use this
+          if cont.get('og:title', False) is not False:
             msgs += "> "+str(cont.get('og:site-title', ''))+"\n> ### ["+str(cont.get('og:title', ''))+"]("+str(url_str)+")\n> "+str(embed_desc)
+          # If there is not an og:title, but there is an image, use this
           elif cont.get('og:title', True) and cont['og:image']:
             msgs += "> "+str(cont.get('og:site-title', ''))+"\n> "+str(embed_desc)
           
-          if cont.get('og:image', False):
+          if cont.get('og:image', False) is not False:
             try:
-              embed_img = HTTPAPI.get_download_url(cont.get('og:image', ''))
-            except TypeError:
+              mauApi = mautrix.api.HTTPAPI('https://matrix.org')
+              # download_type='thumbnail' doesn't work because it requires width/height params, but they don't do anything?
+              embed_img = str(mauApi.get_download_url(cont.get('og:image', False)))
+              # embed_img = str(mauApi.get_download_url(cont.get('og:image', False), download_type='thumbnail')) + "?width=50&height=50"
+            except Exception as e:
+              print(e)
               embed_img = url_str
-            msgs += "\n> ### ![[Click to view image >]({})](".format(url_str) + str(embed_img)+")\n"
+            finally:
+              msgs += "\n> ### ![[Click to view image >]({})](".format(url_str) + str(embed_img) + ")\n\n"
+          
+          # implement MAX_LINKS
+          count += 1
 
-        if len(msgs) == 0:
-            return
+        if count <= 0:
+          await evt.react("💨")
+          return
         await evt.reply(str(msgs), allow_html=True)
