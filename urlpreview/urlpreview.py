58fb3f648efdaefbf6ca40ae4d0132f0fc775978
peterrus <1117858+peterrus@users.noreply.github.com>
2023-08-23 21:02:30 +0200

    fix indenting

--- urlpreview/urlpreview.py
+++ urlpreview/urlpreview.py
@@ -8,6 +8,7 @@ import json
 from typing import List, Type
 import urllib.parse
 
+
 class Config(BaseProxyConfig):
     def do_update(self, helper: ConfigUpdateHelper) -> None:
         helper.copy("appid")
@@ -16,6 +17,7 @@ class Config(BaseProxyConfig):
         helper.copy("min_image_width")
         helper.copy("max_image_embed")
         helper.copy("no_results_react")
+
 
 class UrlpreviewBot(Plugin):
     async def start(self) -> None:
@@ -39,46 +41,49 @@ class UrlpreviewBot(Plugin):
         msgs = ""
         count = 0
         for _, url_str in matches:
-          if count >= MAX_LINKS:
-            break
+            if count >= MAX_LINKS:
+                break
 
-          url_params = urllib.parse.urlencode({"i": url_str, "appid": appid})
-          embed_content = f"https://{HOMESERVER}/_matrix/media/r0/preview_url?url={url_str}"
-          resp = await self.http.get(embed_content, headers={"Authorization":"Bearer {}".format(appid)})
+            url_params = urllib.parse.urlencode({"i": url_str, "appid": appid})
+            embed_content = f"https://{HOMESERVER}/_matrix/media/r0/preview_url?url={url_str}"
+            resp = await self.http.get(embed_content, headers={"Authorization": "Bearer {}".format(appid)})
 
-          # Guard clause
-          if resp.status != 200:
-            continue
-          cont = json.loads(await resp.read())
+            # Guard clause
+            if resp.status != 200:
+                continue
+            cont = json.loads(await resp.read())
 
-          # embed_site_title = ""
-          # if cont.get("og:site-title", None):
-          #   embed_site_title = "<em>"+cont.get("og:site-title", "")+"</em>"
+            # embed_site_title = ""
+            # if cont.get("og:site-title", None):
+            #   embed_site_title = "<em>"+cont.get("og:site-title", "")+"</em>"
 
-          title = None
-          if cont.get("og:title", None):
-            title = f'<h3><a href="{url_str}">{cont.get("og:title", "")}</a></h3>'
-          elif cont.get("og:site-title", None):
-            title = f'<h3><a href="{url_str}">{cont.get("og:site-title", "")}</a></h3>'
+            title = None
+            if cont.get("og:title", None):
+                title = f'<h3><a href="{url_str}">{cont.get("og:title", "")}</a></h3>'
+            elif cont.get("og:site-title", None):
+                title = f'<h3><a href="{url_str}">{cont.get("og:site-title", "")}</a></h3>'
 
-          description = None
-          if cont.get("og:description", None):
-            description = '<p>'+str(cont.get('og:description', '')).replace('\r', ' ').replace('\n', ' ')+'</p>'
+            description = None
+            if cont.get("og:description", None):
+                description = '<p>' + \
+                    str(cont.get('og:description', '')).replace(
+                        '\r', ' ').replace('\n', ' ')+'</p>'
 
-          image = None
-          if cont.get("og:image", None):
-            if (MIN_IMAGE_WIDTH <= 0) or (cont.get("og:image:width", None) and cont.get("og:image:width", 0) > MIN_IMAGE_WIDTH):
-              mauApi = mautrix.api.HTTPAPI("https://"+HOMESERVER)
-              image_url = str(mauApi.get_download_url(cont.get('og:image', None)))
-              image = f'<a href="{image_url}"><img src="{cont.get("og:image", None)}" width="{MAX_IMAGE_EMBED}" alt="Banner image" /></a>'
+            image = None
+            if cont.get("og:image", None):
+                if (MIN_IMAGE_WIDTH <= 0) or (cont.get("og:image:width", None) and cont.get("og:image:width", 0) > MIN_IMAGE_WIDTH):
+                    mauApi = mautrix.api.HTTPAPI("https://"+HOMESERVER)
+                    image_url = str(mauApi.get_download_url(
+                        cont.get('og:image', None)))
+                    image = f'<a href="{image_url}"><img src="{cont.get("og:image", None)}" width="{MAX_IMAGE_EMBED}" alt="Banner image" /></a>'
 
-          msgs += "<blockquote>"
-          msgs += "".join(filter(None, [title, description, image]))
-          msgs += "</blockquote>"
-          count += 1 # Implement MAX_LINKS
+            msgs += "<blockquote>"
+            msgs += "".join(filter(None, [title, description, image]))
+            msgs += "</blockquote>"
+            count += 1  # Implement MAX_LINKS
 
         if count <= 0:
-          if NO_RESULTS_REACT:
-            await evt.react(NO_RESULTS_REACT)
-          return
+            if NO_RESULTS_REACT:
+                await evt.react(NO_RESULTS_REACT)
+            return
         await evt.reply(str(msgs), allow_html=True)
